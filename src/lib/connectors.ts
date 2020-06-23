17a4cc2f72ffd352013e533936c7da8d95b31a46
Taras Shchybovyk <tshchybo@gmail.com>
2020-06-23 20:06:54 +0300

    add new env variable.

--- src/lib/connectors.ts
+++ src/lib/connectors.ts
@@ -1,5 +1,10 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
+import { map } from 'lodash/fp';
+
+const REACT_APP_NETWORKS = map(parseInt)(
+  process.env.REACT_APP_NETWORKS.split(',')
+);
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 5],
+  supportedChainIds: REACT_APP_NETWORKS as any,
 });
